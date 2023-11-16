9120bed3bf1e395c16ca1630434d6990bbe61a3b
eremenko-dd <eremenko-dd@yandex.ru>
2023-11-16 12:36:38 +0400

    довавление комментария

--- work1.py
+++ work1.py
@@ -4,3 +4,6 @@ for i in range(int(input())):
     if text[0].lower() not in ("а","б","в"):
         foo = False
 print('YES' if foo else "NO")
+"""
+Программа проверяет заданное количество введённых слов, начинаются ли они на "а","б","в" или нет
+"""
